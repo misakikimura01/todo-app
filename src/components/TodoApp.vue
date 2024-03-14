497b502dc3ff245fbf44c2666a1bb4025581e6ea
misakikimura01 <misaki.kimura@divx.co.jp>
2024-03-14 17:13:56 +0900

    todoapp commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h1>My TODO App</h1>
+      <input v-model="newTodo" @keyup.enter="addTodo">
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index">
+          {{ todo }}
+          <button @click="removeTodo(index)">Delete</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newTodo: '',
+        todos: []
+      };
+    },
+    methods: {
+      addTodo() {
+        if (this.newTodo.trim()) {
+          this.todos.push(this.newTodo);
+          this.newTodo = '';
+        }
+      },
+      removeTodo(index) {
+        this.todos.splice(index, 1);
+      }
+    }
+  };
+  </script>
+  
